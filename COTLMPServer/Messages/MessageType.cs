bbdddb361d53d15e44d83bae9c2444ed0667f5ff
necoarcc <99202510+necoarcc@users.noreply.github.com>
2025-08-05 15:29:44 +0700

    Message class

---
+++ COTLMPServer/Messages/MessageType.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * PROJECT:     Cult of the Lamb Multiplayer Mod
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     Define the MessageTypes enum
+ * COPYRIGHT:	Copyright 2025 Neco-Arc <neco-arc@inbox.ru>
+ */
+
+/* CLASSES & CODE *************************************************************/
+
+/**
+ * @brief
+ * Contains all classes/structs/enums associated with network messages
+ */
+namespace COTLMPServer.Messages
+{
+    /**
+     * @brief
+     * Message type enum
+     */
+    public enum MessageType
+    {
+        Test
+    }
+}
+
+/* EOF */
