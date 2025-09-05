1ab204fe281c6f3187d55875b6c7d9651f5f6b07
GeoB99 <geobman1999@gmail.com>
2025-09-05 10:16:25 +0200

    [DEBUG] Document the code

--- COTLMP/Debug/Assertions.cs
+++ COTLMP/Debug/Assertions.cs
@@ -13,10 +13,42 @@ using System.Diagnostics;
 
 /* CLASSES & CODE *************************************************************/
 
+/*
+ * @brief
+ * Contains the classes and code for the debugging mechanism
+ * for the mod.
+ * 
+ * @class Assertions
+ * Implements debug assertions.
+ */
 namespace COTLMP.Debug
 {
     public class Assertions
     {
+        /*
+         * @brief
+         * Checks a condition of which the predicate of the said condition
+         * returns TRUE, otherwise the system will stop execution of the mod.
+         * 
+         * @param[in] ConditionToCheck
+         * The condition to check.
+         * 
+         * @param[in] ShowGui
+         * If set to TRUE, the assertion mechanism will display a GUI box with
+         * detailed information of the assertion failure. If set to FALSE, it
+         * won't display any GUI box.
+         * 
+         * @param[in] Message
+         * A string to a message explaining the assertion failure.
+         * 
+         * @param[in] Description
+         * A string to a detailed description which describes further the
+         * failure of the assertion.
+         * 
+         * @remarks
+         * If the caller wants to display an informative GUI box (ShowGui == TRUE),
+         * both Message and Description parameters must be provided.
+         */
         public static void Assert(bool ConditionToCheck, bool ShowGui, string Message, string Description)
         {
             /* Don't display any GUI if the caller hasn't asked for it */
