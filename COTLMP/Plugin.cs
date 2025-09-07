GeoB99/COTLMP

commit 5914e4903e04d70ddabb02f967fd5ead3bc5f328

[COTLMP] Move the patching procedure on appropriate methods
Execute `PatchAll` in OnEnable after the mod has fully initialized its core stuff and unload it in OnDisable.
Count the number of patched methods as well.

COTLMP/Plugin.cs
@@ -16,6 +16,7 @@ using COTLMP.Ui;
 using HarmonyLib;
 using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 /* NAMESPACES *****************************************************************/
@@ -42,6 +43,7 @@ public class Plugin : BaseUnityPlugin
     internal static new ConfigFile Config;
     internal static ModDataGlobals Globals;
     internal static InternalData GlobalsInternal;
+    private readonly Harmony HarmonyManager = new(MyPluginInfo.PLUGIN_GUID);
 
     /*
      * @brief
@@ -54,9 +56,6 @@ public class Plugin : BaseUnityPlugin
         int MaxPlayers;
         string ServerName, PlayerName, GameMode;
         bool ToggleMod, VoiceChat;
-
-        /* Start patching the game assembly with our code */
-        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 
         /* Cache the plugin class methods so that the COTL MP mod can use them across different modules */
         Logger = base.Logger;
@@ -176,6 +175,29 @@ public class Plugin : BaseUnityPlugin
 
         /* Log to the debugger that our mod is loaded */
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    /*
+     * @brief
+     * Patches the game assembly with Harmony patches set up
+     * by the mod.
+     */
+    private void OnEnable()
+    {
+        HarmonyManager.PatchAll(Assembly.GetExecutingAssembly());
+        Logger.LogMessage($"{HarmonyManager.GetPatchedMethods().Count()} patches have been applied!");
+    }
+
+    /*
+     * @brief
+     * Unloads all the patches hooked into game on quit event
+     * of the game.
+     */
+    private void OnDisable()
+    {
+        Logger.LogMessage($"Unloading {MyPluginInfo.PLUGIN_GUID}");
+        HarmonyManager.UnpatchSelf();
+        Logger.LogMessage("Mod has been unloaded!");
     }
 }
 

commit 7f35b14e94b75cdea73167f1fbe4eedabcea836d

[COTLMP] Fix some bugs
- Fix a regression (introduced by `5914e49`) of which the settings locale strings couldn't be retrieved
- Fix a NULL reference exception where StartCoroutine expected a IEnumerator routine to be passed in the `BannerEditionPatch` patch

COTLMP/Plugin.cs
@@ -43,7 +43,7 @@ public class Plugin : BaseUnityPlugin
     internal static new ConfigFile Config;
     internal static ModDataGlobals Globals;
     internal static InternalData GlobalsInternal;
-    private readonly Harmony HarmonyManager = new(MyPluginInfo.PLUGIN_GUID);
+    private Harmony HarmonyInstance;
 
     /*
      * @brief
@@ -56,6 +56,12 @@ public class Plugin : BaseUnityPlugin
         int MaxPlayers;
         string ServerName, PlayerName, GameMode;
         bool ToggleMod, VoiceChat;
+
+        /*
+         * Start patching the game assembly with our code
+         * and cache a harmony instance of this mod.
+         */
+        HarmonyInstance = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 
         /* Cache the plugin class methods so that the COTL MP mod can use them across different modules */
         Logger = base.Logger;
@@ -80,7 +86,7 @@ public class Plugin : BaseUnityPlugin
         if (SettingData == null)
         {
             Logger.LogFatal("Failed to set default or load the \"Mod Toggle\" setting!");
-            Harmony.UnpatchAll();
+            HarmonyInstance.UnpatchSelf();
             return;
         }
 
@@ -95,7 +101,7 @@ public class Plugin : BaseUnityPlugin
         if (SettingData == null)
         {
             Logger.LogFatal("Failed to set default or load the \"Game Mode\" setting!");
-            Harmony.UnpatchAll();
+            HarmonyInstance.UnpatchSelf();
             return;
         }
 
@@ -110,7 +116,7 @@ public class Plugin : BaseUnityPlugin
         if (SettingData == null)
         {
             Logger.LogFatal("Failed to set default or load the \"Player Name\" setting!");
-            Harmony.UnpatchAll();
+            HarmonyInstance.UnpatchSelf();
             return;
         }
 
@@ -125,7 +131,7 @@ public class Plugin : BaseUnityPlugin
         if (SettingData == null)
         {
             Logger.LogFatal("Failed to set default or load the \"Server Name\" setting!");
-            Harmony.UnpatchAll();
+            HarmonyInstance.UnpatchSelf();
             return;
         }
 
@@ -140,7 +146,7 @@ public class Plugin : BaseUnityPlugin
         if (SettingData == null)
         {
             Logger.LogFatal("Failed to set default or load the \"Max Players\" setting!");
-            Harmony.UnpatchAll();
+            HarmonyInstance.UnpatchSelf();
             return;
         }
 
@@ -155,7 +161,7 @@ public class Plugin : BaseUnityPlugin
         if (SettingData == null)
         {
             Logger.LogFatal("Failed to set default or load the \"Toggle Voice Chat\" setting!");
-            Harmony.UnpatchAll();
+            HarmonyInstance.UnpatchSelf();
             return;
         }
 
@@ -179,13 +185,12 @@ public class Plugin : BaseUnityPlugin
 
     /*
      * @brief
-     * Patches the game assembly with Harmony patches set up
-     * by the mod.
+     * Performs additional tasks after the successful initialization
+     * of the mod.
      */
     private void OnEnable()
     {
-        HarmonyManager.PatchAll(Assembly.GetExecutingAssembly());
-        Logger.LogMessage($"{HarmonyManager.GetPatchedMethods().Count()} patches have been applied!");
+        Logger.LogMessage($"{HarmonyInstance.GetPatchedMethods().Count()} patches have been applied!");
     }
 
     /*
@@ -196,7 +201,7 @@ public class Plugin : BaseUnityPlugin
     private void OnDisable()
     {
         Logger.LogMessage($"Unloading {MyPluginInfo.PLUGIN_GUID}");
-        HarmonyManager.UnpatchSelf();
+        HarmonyInstance.UnpatchSelf();
         Logger.LogMessage("Mod has been unloaded!");
     }
 }

COTLMP/Ui/Banner.cs
@@ -8,12 +8,12 @@
 /* IMPORTS ********************************************************************/
 
 using COTLMP;
-using COTLMP.Debug;
 using COTLMP.Data;
 using BepInEx;
 using HarmonyLib;
 using I2.Loc;
 using Lamb.UI;
+using System.Collections;
 
 /* CLASSES & CODE *************************************************************/
 
@@ -30,6 +30,17 @@ namespace COTLMP.Ui
     [HarmonyPatch]
     internal static class Banner
     {
+
+        /*
+         * @brief
+         * A dummy IEnumerator method that is used to replace the
+         * returned value which represents a coroutine.
+         */
+        private static IEnumerator BannerEnumerator()
+        {
+            yield break;
+        }
+
         /*
          * @brief
          * Patches the private DLC check edition method, of which it replaces
@@ -38,13 +49,17 @@ namespace COTLMP.Ui
          * @param[in] __instance
          * The current instance value of the method being patched.
          * 
+         * @param[in] __result
+         * The current result value being returned. Typically the original
+         * method returns a IEnumerator.
+         * 
          * @return
          * Returns TRUE if tthe original method of the game is to be executed.
          * FALSE if our method is to be executed instead.
          */
         [HarmonyPatch(typeof(ShowIfSpecialEdition), "WaitForDLCCheck")]
         [HarmonyPrefix]
-        private static bool BannerEditionPatch(ShowIfSpecialEdition __instance)
+        private static bool BannerEditionPatch(ShowIfSpecialEdition __instance, ref IEnumerator __result)
         {
             /* The mod is currently not executing so run the original method instead */
             if (!Plugin.Globals.EnableMod)
@@ -55,6 +70,7 @@ namespace COTLMP.Ui
             /* Replace the banner header */
             __instance._localize.Term = "UI/Banner";
             __instance._text.text = MultiplayerModLocalization.UI.Multiplayer_Banner;
+            __result = BannerEnumerator();
             return false;
         }
     }