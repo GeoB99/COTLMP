aac5a970b348756a21820c84bbae502853cf8bb3
GeoB99 <geobman1999@gmail.com>
2026-03-03 23:40:21 +0100

    [COTLMP:UI] Implement the Say chat box

---
+++ COTLMP/Ui/SayChat.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * PROJECT:     Cult of the Lamb Multiplayer Mod
+ * LICENSE:     MIT (https://spdx.org/licenses/MIT)
+ * PURPOSE:     Say chat box support
+ * COPYRIGHT:	Copyright 2026 GeoB99 <geobman1999@gmail.com>
+ */
+
+/* IMPORTS ********************************************************************/
+
+using COTLMP;
+using COTLMP.Api;
+using COTLMP.Data;
+using COTLMP.Debug;
+using HarmonyLib;
+using BepInEx;
+using I2.Loc;
+using TMPro;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* CLASSES & CODE *************************************************************/
+
+namespace COTLMP.Ui
+{
+    public static class SayChat
+    {
+        /* TODO */
+    }
+}
+
+/* EOF */
