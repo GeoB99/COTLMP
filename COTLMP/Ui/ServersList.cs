398a5d33b64e7640d670750d1dbc0bc231a08343
GeoB99 <geobman1999@gmail.com>
2026-07-26 00:01:38 +0200

    [COTLMP:UI] Implement LAN server querying and miscellaneous stuff
    - Implement QueryLanResponse and FetchServerInfo methods to fetch any reachable server and create entries for them in GUI
    - Implement IsServerEntryPresent to avoid duplicating server entries of the same kind
    - SetServerBrowserStatus(): Don't update the server browser status if it's been hidden

--- COTLMP/Ui/ServersList.cs
+++ COTLMP/Ui/ServersList.cs
@@ -11,19 +11,25 @@ using COTLMP;
 using COTLMP.Api;
 using COTLMP.Data;
 using COTLMP.Debug;
+using COTLMP.Network;
+using static COTLMP.Data.Network;
+using COTLMPServer.Messages;
 using HarmonyLib;
 using BepInEx;
 using BepInEx.Configuration;
 using I2.Loc;
-using System.Collections;
 using System;
-using System.Net;
-using System.Threading;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static COTLMP.Data.Network;
 
 /* CLASSES & CODE *************************************************************/
 
@@ -44,6 +50,11 @@ namespace COTLMP.Ui
         private static TMP_Dropdown ServerCategories;
         private static LinkedList<ServerEntry> ServerEntries;
         private static LinkedList<ServerEntry> ServerLanEntries;
+        private static LinkedList<ServerEntry> CurrentServerList;
+        private static UdpClient ClientReceiver;
+        private static IPAddress MulticastIp;
+        private static IPEndPoint MulticastEndPoint;
+        private static CancellationTokenSource LanToken = null;
         private static readonly object ServerListLock = new object();
 
         /// <summary>
@@ -125,6 +136,17 @@ namespace COTLMP.Ui
                 ServerLanEntries.Clear();
             }
 
+            /* Cleanup the serverlist client */
+            ClientReceiver.DropMulticastGroup(MulticastIp);
+            ClientReceiver.Close();
+
+            /* Cleanup the serverlist's LAN CTS as we no longer need it */
+            if (LanToken != null)
+            {
+                LanToken.Cancel();
+                LanToken.Dispose();
+            }
+
             /* Return to the main menu of the game */
             COTLMP.Api.Assets.ShowScene("Main Menu", false, null);
         }
@@ -164,6 +186,12 @@ namespace COTLMP.Ui
         {
             string StatusMessage;
             COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.MESSAGE_LEVEL, DebugComponent.UI_COMPONENT, "SetServerStatus() called");
+
+            /* Don't update the browser status if it's already been hidden and the caller wants to hide it again */
+            if (ServerBrowserStatus.gameObject.activeSelf == false && !DisplayStatus)
+            {
+                return;
+            }
 
             /* Determine which server status message to be displayed */
             switch (Status)
@@ -303,6 +331,7 @@ namespace COTLMP.Ui
              */
             if (WhatToRefresh == REFRESH_WHAT.InternetList)
             {
+                CurrentServerList = ServerEntries;
                 if (ServerLanEntries != null)
                 {
                     lock (ServerListLock)
@@ -318,9 +347,18 @@ namespace COTLMP.Ui
 
                 /* FIXME: Internet servers lookup (also no masterserver connection) isn't implemented yet */
                 SetServerBrowserStatus(SERVER_BROWSER_STATUS.MasterserverConnectFail, true);
+
+                /* We switched away from the LAN list, cancel any related operations to LAN querying */
+                if (LanToken != null)
+                {
+                    LanToken.Cancel();
+                    LanToken.Dispose();
+                    LanToken = null;
+                }
             }
             else
             {
+                CurrentServerList = ServerLanEntries;
                 if (ServerEntries != null)
                 {
                     lock (ServerListLock)
@@ -334,9 +372,264 @@ namespace COTLMP.Ui
                     }
                 }
 
-                /* FIXME: LAN servers lookup isn't implemented yet */
-                SetServerBrowserStatus(SERVER_BROWSER_STATUS.NoneFound, true);
+                /* Recreate the LAN CTS if we switched away from the LAN list */
+                if (LanToken == null)
+                {
+                    LanToken = new CancellationTokenSource();
+                }
+
+                /*
+                 * Start listening to any broadcast signal the LAN servers might
+                 * send us and fetch their server info so we can populate the
+                 * serverlist GUI. The scan progress status will be updated
+                 * as soon as it finds a reachable LAN server.
+                 *
+                 * TODO: We should periodically ping every cached server entry to
+                 * ensure they're still reachable and discard any entry that's
+                 * no longer online. Maybe for a future PR.
+                 */
+                SetServerBrowserStatus(SERVER_BROWSER_STATUS.ScanInProgress, true);
+                _ = System.Threading.Tasks.Task.Run(QueryLanResponse);
             }
+        }
+
+        /// <summary>
+        /// Periodically queries for broadcast response from any reacheable server
+        /// within the LAN network and fetches their info data into the servers
+        /// browser GUI.
+        /// </summary>
+        private static async System.Threading.Tasks.Task QueryLanResponse()
+        {
+            UdpReceiveResult Result;
+
+            COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.MESSAGE_LEVEL, DebugComponent.UI_COMPONENT, "QueryLanResponse() called");
+
+            while (CurrentServerList == ServerLanEntries)
+            {
+                try
+                {
+                    /* Let the task wait indefinitely for a response, abort it if the server suddenly stopped communicating with us */
+                    using var Cts = CancellationTokenSource.CreateLinkedTokenSource(LanToken.Token);
+                    var TimeOut = System.Threading.Tasks.Task.Delay(Timeout.Infinite, Cts.Token);
+
+                    /* Wait for a response from the server to broadcast its presence */
+                    var Recv = ClientReceiver.ReceiveAsync();
+                    if (await System.Threading.Tasks.Task.WhenAny(TimeOut, Recv) == TimeOut)
+                    {
+                        return;
+                    }
+
+                    /* Grab the response result from the server */
+                    Cts.Cancel();
+                    Result = await Recv;
+
+                    /*
+                     * The user suddenly switched server category while we were querying
+                     * for LAN server responses in the middle. Abort this task.
+                     */
+                    if (CurrentServerList != ServerLanEntries)
+                    {
+                        return;
+                    }
+
+                    /* Ofc bail out if this server has already been added and continue querying for response */
+                    if (IsServerEntryPresent(Result.RemoteEndPoint, true))
+                    {
+                        COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.WARNING_LEVEL, DebugComponent.UI_COMPONENT,
+                                                              $"Server with port {Result.RemoteEndPoint.Port} has already been inserted into serverlist, bail out this one!");
+                        return;
+                    }
+
+                    /* Service this LAN response from the server with a Unity coroutine and fetch its server info */
+                    Plugin.MonoInstance.StartCoroutine(FetchServerInfo(Result, true));
+                }
+                catch (TaskCanceledException)
+                {
+                    COTLMP.Debug.PrintLogger.Print(DebugLevel.WARNING_LEVEL, DebugComponent.NETWORK_STACK_COMPONENT, "Stop querying for LAN response, we are no longer in the LAN list");
+                }
+                catch (Exception ex)
+                {
+                    COTLMP.Debug.PrintLogger.Print(DebugLevel.FATAL_LEVEL, DebugComponent.UI_COMPONENT,
+                                                   $"Broadcast response ABORTED: {ex.GetType()} + ERROR MESSAGE: {ex.Message} + GUILTY METHOD: {ex.TargetSite}");
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if a server entry has already been created.
+        /// </summary>
+        /// <param name = "ServerEndPoint">The IP endpoint of the target server to look for.</param>
+        /// <param name = "SearchInLan">Set it to TRUE if the search should be done in the LAN server entries, FALSE otherwise.</param>
+        /// <returns>Returns TRUE if the server entry is already created, otherwise FALSE.</returns>
+        private static bool IsServerEntryPresent(IPEndPoint ServerEndPoint, bool SearchInLan)
+        {
+            bool IsPresent = false;
+
+            lock (ServerListLock)
+            {
+                if (SearchInLan)
+                {
+                    foreach (ServerEntry Entry in ServerLanEntries)
+                    {
+                        if (Entry.Address.Equals(ServerEndPoint.Address))
+                        {
+                            IsPresent = true;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (ServerEntry Entry in ServerEntries)
+                    {
+                        if (Entry.Address.Equals(ServerEndPoint.Address))
+                        {
+                            IsPresent = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return IsPresent;
+        }
+
+        /// <summary>
+        /// Fetches the server info from the retrieved server response and
+        /// creates a server GUI entry.
+        /// </summary>
+        /// <param name = "Result">The result response we got from a server of which we have to fetch its info from.</param>
+        /// <param name = "IsLan">Set it to TRUE if the server is bound within LAN network and it should be added in LAN servers list, FALSE otherwise.</param>
+        private static IEnumerator FetchServerInfo(UdpReceiveResult Result, bool IsLan)
+        {
+            ServerEntry Entry;
+            IPEndPoint ServerEndPoint;
+            Message SrvInfoMsgRequest, Msg;
+            ServerInfo SrvInfo;
+            UdpReceiveResult InfoResult;
+            uint ServerPort;
+            var Wait = new WaitForTask(null);
+
+            COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.MESSAGE_LEVEL, DebugComponent.UI_COMPONENT, "FetchServerInfo() called");
+
+            /*
+             * The server we got a response from is alive and reachable.
+             * Not all platforms return a little endian bit so convert the
+             * server port into little endian before using it.
+             */
+            ServerPort = BitConverter.ToUInt32(Result.Buffer, 0);
+            if (!BitConverter.IsLittleEndian)
+            {
+                ServerPort = COTLMPServer.Utils.ReverseEndianness(ServerPort);
+            }
+
+            /*
+             * We must send a message request telling the server we need their info
+             * so we can populate the serverlist.
+             */
+            COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.MESSAGE_LEVEL, DebugComponent.UI_COMPONENT, $"Sending server info request to port -> {ServerPort}");
+            ServerEndPoint = new IPEndPoint(Result.RemoteEndPoint.Address, (int)ServerPort);
+            SrvInfoMsgRequest = new Message(MessageType.ServerInfo, 0, null);
+
+            /*
+             * We cannot use the multicast client (aka the ClientReceiver) of the serverlist
+             * because it's already being used by another thread from a task. Two threads waiting
+             * to receive datagrams can lead to race conditions and we might receive whatever
+             * packet we get. So setup a temporary client for the serverlist that ONLY connects
+             * to the reachable server and request its info from.
+             */
+            using var FetchClient = new UdpClient();
+            FetchClient.Client.Connect(ServerEndPoint);
+
+            Wait.what = FetchClient.SendAsync(SrvInfoMsgRequest.Serialize(), SrvInfoMsgRequest.Serialize().Length);
+            yield return Wait;
+            if (Wait.what.IsFaulted || Wait.what.IsCanceled)
+            {
+                COTLMP.Debug.PrintLogger.Print(DebugLevel.ERROR_LEVEL, DebugComponent.UI_COMPONENT, "Sending server info request message failed!");
+                yield break;
+            }
+
+            /*
+             * As we shipped the server info request message we now wait to receive an
+             * answer back. Put a timeout of 10 seconds, if the server doesn't respond
+             * to us in time then punt this operation.
+             */
+            using var Cts = CancellationTokenSource.CreateLinkedTokenSource(LanToken.Token);
+            var Timeout = System.Threading.Tasks.Task.Delay(10000, Cts.Token);
+
+            var Recv = FetchClient.ReceiveAsync();
+            var WaitResult = System.Threading.Tasks.Task.WhenAny(Timeout, Recv);
+            Wait.what = WaitResult;
+            yield return Wait;
+
+            if (Recv.IsFaulted || Recv.IsCanceled)
+            {
+                COTLMP.Debug.PrintLogger.Print(DebugLevel.ERROR_LEVEL, DebugComponent.UI_COMPONENT, "Receiving server info failed!");
+                yield break;
+            }
+
+            if (WaitResult.Result == Timeout)
+            {
+                COTLMP.Debug.PrintLogger.Print(DebugLevel.ERROR_LEVEL, DebugComponent.UI_COMPONENT, "Receiving server info has timed out!");
+                yield break;
+            }
+
+            /* Cache the info result from the packet we just received */
+            Cts.Cancel();
+            InfoResult = Recv.Result;
+
+            /* FIXME: Check that the IP address is within LAN range before we trust it, this is for a future PR */
+
+            try
+            {
+                /* We received a message from the server, make sure the sequence isn't botched */
+                COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.MESSAGE_LEVEL, DebugComponent.UI_COMPONENT, $"Serverinfo received back form port -> {ServerPort}, fetching data...");
+                Msg = COTLMPServer.Messages.Message.Deserialize(InfoResult.Buffer);
+                if (Msg.Sequence != 1)
+                {
+                    COTLMP.Debug.PrintLogger.Print(DebugLevel.WARNING_LEVEL, DebugComponent.UI_COMPONENT, $"The mesage has invalid sequence (expected 1, got {Msg.Sequence}!");
+                    yield break;
+                }
+
+                /* Fetch the info from the message buffer and create a server entry with it */
+                SrvInfo = ServerInfo.Deserialize(Msg.Data);
+            }
+            catch (InvalidDataException)
+            {
+                COTLMP.Debug.PrintLogger.Print(DebugLevel.WARNING_LEVEL, DebugComponent.UI_COMPONENT, "Invalid server data, bail out!");
+                yield break;
+            }
+            catch (Exception ex)
+            {
+                COTLMP.Debug.PrintLogger.Print(DebugLevel.FATAL_LEVEL, DebugComponent.UI_COMPONENT,
+                                               $"Fetching server info FAILED: {ex.GetType()} + ERROR MESSAGE: {ex.Message} + GUILTY METHOD: {ex.TargetSite}");
+                yield break;
+            }
+
+            COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.MESSAGE_LEVEL, DebugComponent.UI_COMPONENT, $"Server name -> {SrvInfo.ServerName}");
+            COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.MESSAGE_LEVEL, DebugComponent.UI_COMPONENT, $"Active players -> {SrvInfo.ActivePlayers}");
+            COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.MESSAGE_LEVEL, DebugComponent.UI_COMPONENT, $"Max players -> {SrvInfo.MaxPlayers}");
+            COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.MESSAGE_LEVEL, DebugComponent.UI_COMPONENT, $"Server port -> {ServerEndPoint.Port}");
+            COTLMP.Debug.PrintLogger.PrintVerbose(DebugLevel.MESSAGE_LEVEL, DebugComponent.UI_COMPONENT, $"Server IP -> {ServerEndPoint.Address}");
+
+            /* Hide the browser server status, we got a server entry */
+            SetServerBrowserStatus(SERVER_BROWSER_STATUS.ScanInProgress, false);
+
+            Entry = CreateServerEntry(SrvInfo.ServerName,
+                                      ServerEndPoint.Address,
+                                      IsLan,
+                                      (ushort)ServerEndPoint.Port,
+                                      COTLMPServer.Data.GameModes.TranslateGameModeToString(SrvInfo.Mode),
+                                      SrvInfo.ActivePlayers,
+                                      SrvInfo.MaxPlayers);
+            if (Entry == null)
+            {
+                COTLMP.Debug.PrintLogger.Print(DebugLevel.FATAL_LEVEL, DebugComponent.UI_COMPONENT,
+                                               $"Failed to create a server entry (IP: {ServerEndPoint.Address}, Port: {ServerEndPoint.Port}");
+            }
+
+            yield break;
         }
 
         /// <summary>
@@ -507,9 +800,37 @@ namespace COTLMP.Ui
             /* Load the server list UI scene, the asset bundle should be already loaded */
             COTLMP.Api.Assets.ShowScene("ServerListUI", false, null);
 
-            /* Initialize the server entries list head */
+            /* Setup the LAN CTS for the serverlist for any cancelable operations for LAN servers */
+            LanToken = new CancellationTokenSource();
+
+            /* Initialize the server entries list heads */
             ServerEntries = new LinkedList<ServerEntry>();
             ServerLanEntries = new LinkedList<ServerEntry>();
+            CurrentServerList = ServerEntries;
+
+            /* Initialize the broadcast scanning constructs */
+            ClientReceiver = new UdpClient(AddressFamily.InterNetwork);
+            MulticastIp = IPAddress.Parse("239.15.7.11");
+            MulticastEndPoint = new IPEndPoint(IPAddress.Any, 1175);
+
+            /*
+             * Setup the server list client listener. This will be used to listen
+             * to upcoming servers thorough heartbeats between client and server
+             * and populate the browser list.
+             */
+            try
+            {
+                ClientReceiver.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                ClientReceiver.ExclusiveAddressUse = false;
+                ClientReceiver.Client.Bind(MulticastEndPoint);
+                ClientReceiver.JoinMulticastGroup(MulticastIp);
+            }
+            catch (Exception ex)
+            {
+                COTLMP.Debug.PrintLogger.Print(DebugLevel.FATAL_LEVEL, DebugComponent.UI_COMPONENT,
+                                               $"Serverlist client init FAILED: {ex.GetType()} + ERROR MESSAGE: {ex.Message} + GUILTY METHOD: {ex.TargetSite}");
+                return;
+            }
 
             /*
              * Invoke the UI initialization worker with a coroutine. Unity loads
